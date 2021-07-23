421734c1f0bfb6bb36c71c754406173679abbd74
Your Name <you@example.com>
2021-07-23 15:44:18 -0500

    route to new page, display list of open issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,9 +1,27 @@
+var repoNameEl = document.querySelector("#repo-name");
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
 
+
+var getRepoName = function () {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName){
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+        console.log(repoName);
+    }
+    else{
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    
     fetch(apiUrl).then(function(response) {
         //request was successful
         if (response.ok) {
@@ -18,12 +36,12 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("Ther was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 }
 
-getRepoIssues("facebook/react");
 
 var displayIssues = function(issues){
     //check for no issues to display message
@@ -74,3 +92,5 @@ var displayWarning = function(repo) {
     //append to warning container
     limitWarningEl.appendChild(linkEl);
   }
+  
+getRepoName();
